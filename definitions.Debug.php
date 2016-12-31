dc16806ec49fc449f7af2b0dfcecc26e533057bc
孙海倍 <shb@sunhb.me>
2016-12-31 12:38:14 +0800

    Debug插件中文包

---
+++ definitions.Debug.php
@@ -0,0 +1,10 @@
+<?php
+// Copyright Xs_sunhb
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+//Chinese Definitions for the Debug plugin.
+
+$definitions["MySQL queries"] = "MySQL 查询";
+$definitions["Page loaded in %s seconds"] = "页面载入共%s秒";
+$definitions["POST + GET + FILES information"] = "POST + GET + FILES信息";
+$definitions["SESSION + COOKIE information"] = "SESSION + COOKIE信息";
