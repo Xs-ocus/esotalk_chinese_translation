2818cdd2491a11fd70dad7f37b90b2e199da56fa
孙海倍 <shb@sunhb.me>
2016-12-31 12:50:33 +0800

    Create definitions.SMTP.php

---
+++ definitions.SMTP.php
@@ -0,0 +1,10 @@
+<?php
+// Copyright Xs_sunhb
+// Chinese Definitions for the SMTP plugin.
+
+$definitions["Server"] = "服务器";
+$definitions["Port"] = "端口";
+$definitions["Authentication"] = "验证方式";
+$definitions["Normal"] = "普通";
+$definitions["TLS"] = "TLS";
+$definitions["SSL"] = "SSL";
