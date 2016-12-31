92d2fb9067cbc591b0516c8b21f632bee6afc94a
孙海倍 <shb@sunhb.me>
2016-12-31 12:42:00 +0800

    BBCode插件中文包

---
+++ definitions.BBCode.php
@@ -0,0 +1,12 @@
+<?php
+// Copyright Xs_sunhb
+// Chinese Definitions for the BBCode plugin.
+
+$definitions["Code"] = "代码";
+$definitions["Image"] = "图像";
+$definitions["Link"] = "链接";
+$definitions["Strike"] = "删除线";
+$definitions["Header"] = "标题";
+$definitions["Italic"] = "斜体";
+$definitions["Bold"] = "加粗";
+$definitions["Quote"] = "引用";
